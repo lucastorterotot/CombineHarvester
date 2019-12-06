6a9be86a3f77f716dde4bc2a061d1d641c597e79
lucastorterotot <lucas.torterotot@gmail.com>
2019-12-06 16:15:27 +0100

    Change HttSystematics_MSSMRun2 to Gaels version

--- HTTMSSM2017/interface/HttSystematics_MSSMRun2.h
+++ HTTMSSM2017/interface/HttSystematics_MSSMRun2.h
@@ -1,11 +1,11 @@
-#ifndef MSSM2016_HttSystematics_MSSMRun2_h
-#define MSSM2016_HttSystematics_MSSMRun2_h
+#ifndef MSSM2017_HttSystematics_MSSMRun2_h
+#define MSSM2017_HttSystematics_MSSMRun2_h
 #include "CombineHarvester/CombineTools/interface/CombineHarvester.h"
 
 namespace ch {
-// Run2 MSSM analysis systematics
-// Implemented in src/HttSystematics_MSSMRun2.cc
-void AddMSSMRun2Systematics(CombineHarvester& cb, int control_region = 0, bool zmm_fit = false, bool ttbar_fit = false);
+// Run2 SM analysis systematics
+// Implemented in src/HttSystematics_SMRun2.cc
+void AddMSSMRun2Systematics(CombineHarvester& cb, bool jetfakes, bool embedding, bool ggh_wg1, int era);
 }
 
 #endif
