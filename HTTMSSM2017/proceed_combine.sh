a57ad00389ab639253c64b4b9c1235039e13a1bc
lucastorterotot <lucas.torterotot@gmail.com>
2020-02-21 17:13:00 +0100

    Add r_bbH param in the FitDiagnostics command

--- HTTMSSM2017/proceed_combine.sh
+++ HTTMSSM2017/proceed_combine.sh
@@ -8,7 +8,7 @@ MorphingHTTMSSM2017 --channel=mt --real_data=true
 combineTool.py -M T2W -o "ws.root" -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO '"map=^.*/ggH$:r_ggH[0,0,200]"' --PO '"map=^.*/bbH$:r_bbH[0,0,200]"' -i output/HTT_MSSM_2017_mt/mt/
 
 cd output/HTT_MSSM_2017_mt/
-combine -M FitDiagnostics --saveNLL --numToysForShapes 1000 --verbose 1 -d mt/ws.root -m 600 --saveNormalizations --saveShapes --saveWithUncertainties --saveWorkspace -n test_output --plots  --redefineSignalPOIs r_ggH
+combine -M FitDiagnostics --saveNLL --numToysForShapes 1000 --verbose 1 -d mt/ws.root -m 600 --saveNormalizations --saveShapes --saveWithUncertainties --saveWorkspace -n test_output --plots  --redefineSignalPOIs r_ggH,r_bbH
 
 python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py fitDiagnosticstest_output.root -a -g nuisances_plots.root -p r_bbH -f txt > nuisances.txt
 cp fitDiagnosticstest_output.root fitDiagnosticstest_output_600.root
